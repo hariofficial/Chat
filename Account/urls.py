a3997d474cdfef1a865bd8ce73424cf1bf4adfc5
Prasad <60845868+Prasad040@users.noreply.github.com>
2020-08-07 13:21:06 +0530

    Add files via upload

---
+++ Account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.login, name="login"),
+    path('register.html', views.register, name="register"),
+    path('home.html', views.homepage, name="homepage"),
+]
---
+++ Account/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def homepage(request):
+    return render(request, 'home.html')
